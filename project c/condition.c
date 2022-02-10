74d6f8ed892e13b2413df8e3866375fdf6ca282e
Shubhangi Ganbas <80036108+Shubhangi4099@users.noreply.github.com>
2022-02-10 11:56:19 +0530

    Add files via upload

---
+++ project c/condition.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int fmarks = 40;
+    printf("\n enter aggregate marks:  ");
+   scanf("%f",&fmarks);
+   if (fmarks >=60)
+   {
+       printf("\n you are eligiable.....");
+
+   }
+   else{
+       printf("\n you are not eligiable...");
+
+   }
+   return 0;
+
+
+}
