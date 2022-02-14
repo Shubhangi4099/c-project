70b496f674a339ebb302eeeba7cb506c20795783
Shubhangi Ganbas <80036108+Shubhangi4099@users.noreply.github.com>
2022-02-14 13:20:34 +0530

    Add files via upload

---
+++ project c/if statement.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main ()
+{
+int num;
+printf("\n Enter the number: ");
+scanf("%d",&num);
+if(num%2==0)
+{
+printf("\n The number is even.");
+}
+else
+{
+printf("\n The number is odd.");
+}
+getch();
+}
